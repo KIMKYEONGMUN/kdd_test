e8317824e4d7a513d68780a5d02821d66484888e
KIMKYEONGMUN <139826895+KIMKYEONGMUN@users.noreply.github.com>
2024-03-15 17:02:22 +0900

    Add files via upload

---
+++ js/kdd.js
@@ -0,0 +1,31 @@
+$(function(){
+    $('.header').mouseenter(function(){
+        $(this).addClass('on');
+    });
+    $('.header').mouseleave(function(){
+        $(this).removeClass('on');
+    });
+
+    var swiper = new Swiper(".mainSwiper", {
+        spaceBetween: 0,
+        effect: "fade",
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 9000,
+            disableOnInteraction: false,
+        },
+        on: {
+            slideChange: function(){
+                $('.bar_progress').removeClass('on');
+                console.log($('.bar_progress').offset());
+                $('.bar_progress').addClass('on');
+            }
+        },
+      });
+
+});// jquery end
